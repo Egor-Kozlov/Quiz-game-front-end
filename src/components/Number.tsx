0eb9b6e8657e640e9205c01afe9db862fe8d98a7
IvanovVlad7 <v.ivanov28051998@mail.ru>
2023-06-28 15:43:20 +0200

    added test commit

--- src/components/Number.tsx
+++ src/components/Number.tsx
@@ -2,7 +2,7 @@ import './Number.scss'
 
 
 const Numbers = () => {
-  const numbers = [1, 2, 3, 4, 5]
+  const numbers = [1, 2, 3, 4, 5,4 , 2, 2,]
   return (
     <div className="number-paragraph">
       <p>
