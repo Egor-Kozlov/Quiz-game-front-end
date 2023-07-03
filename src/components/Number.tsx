200de7973671d8fefbaa48030ed4b86600b215f5
IvanovVlad7 <v.ivanov28051998@mail.ru>
2023-07-03 22:06:26 +0200

    added text

--- src/components/Number.tsx
+++ src/components/Number.tsx
@@ -2,10 +2,9 @@
 import './Number.scss';
 import { useEffect, useState } from 'react';
 
-
-
 const Number: React.FC = () => {
   const [numbers, setNumbers] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     fetchData();
@@ -13,17 +12,20 @@ const Number: React.FC = () => {
 
   const fetchData = async () => {
     try {
+      setIsLoading(true);
       const response = await fetch('https://quiz-game-dev.cyclic.app/get-key');
       const data = await response.json();
-      setNumbers(data.numbers);
+      setNumbers(data.value);
     } catch (error) {
       console.error('Ошибка при получении данных:', error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
     <div className="number-paragraph">
-      {numbers}
+      {isLoading ? <div>Loading...</div> : numbers}
     </div>
   );
 };
